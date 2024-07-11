f93f9f9b0f5ea5fbd82d7bc13ac5df377daa8df7
duyhungg <dongocduyhung7@gmail.com>
2024-07-11 21:16:25 +0700

    update

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDatabase = () => {
-  let DB_URI = "mongodb://localhost:27017/TLCN";
+  let DB_URI = "";
 
   if (process.env.NODE_ENV === "DEVELOPMENT") DB_URI = process.env.DB_LOCAL_URI;
   if (process.env.NODE_ENV === "PRODUCTION") DB_URI = process.env.DB_URI;
